a3516108b6d9e4f8e2c762f49007a5bfd932d132
Lingfei Lu <l6lu@ucsd.edu>
2016-03-31 16:50:26 -0700

    Initial Commit

---
+++ cse167_hw1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  cse167_hw1
+//
+//  Created by Simon on 3/31/16.
+//  Copyright © 2016 Simon. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
