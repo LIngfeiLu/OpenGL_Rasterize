97647ecd41d8cc6515369afbbe935ee7592f033f
Lingfei Lu <l6lu@ucsd.edu>
2016-04-11 16:14:51 -0700

    first commit

---
+++ cse167_hw1/Rasterizer.hpp
@@ -0,0 +1,13 @@
+//
+//  Rasterizer.hpp
+//  cse167_hw1
+//
+//  Created by Simon on 4/6/16.
+//  Copyright © 2016 Simon. All rights reserved.
+//
+
+#ifndef Rasterizer_h
+#define Rasterizer_h
+
+
+#endif /* Rasterizer_h */
